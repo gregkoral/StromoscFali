dae0bd2e0f093afe997aee8a6225ec8233638d7e
gregkoral <koralewski.g@gmail.com>
2026-05-28 13:02:37 +0200

    Add files via upload

--- m_wave9.py
+++ m_wave9.py
@@ -87,9 +87,9 @@ cmap_vtm02 = plt.cm.plasma
 
 # --- INTERFEJS UŻYTKOWNIKA (PIONOWY LAYOUT) ---
 # --- WYCENTROWANE NAGŁÓWKI WWW ---
-st.markdown(
-    "<h2 style='text-align: center;'>Prognoza Fal Bałtyku</h1>", 
-    unsafe_allow_html=True
+##st.markdown(
+    ##"<h2 style='text-align: center;'>Prognoza Fal Bałtyku</h1>", 
+    ##unsafe_allow_html=True
 )
 st.markdown(
     f"<h3 style='text-align: center; color: #888888; font-weight: normal; margin-bottom: 20px;'>"
